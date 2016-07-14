9177d5ffa47f636495da9e948c64acfc15acec07
guoshencheng <648772021@qq.com>
2016-07-14 17:40:17 +0800

    use hot rate

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,7 @@ router.get('/map/share/:id', function(req, res, next) {
 router.get('/map/:id', checkCidParam, fetchContent, function(req, res, next) {
   var cid = req.cardId
   var contentInfo = req.contentInfo
+  contentInfo.ratio = parseInt(contentInfo.ratio * 100)
   contentInfo.card.view = contentInfo.card.view || 0
   contentInfo.distance = (contentInfo.distance / 1000).toFixed(1)
   contentInfo.friendPushCount = contentInfo.friendPushCount || 0
