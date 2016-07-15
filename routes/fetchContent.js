d487816a44f921865f02e1c36bdf385692913b88
guoshencheng <648772021@qq.com>
2016-07-15 17:16:36 +0800

    add hottext

--- routes/fetchContent.js
+++ routes/fetchContent.js
@@ -12,6 +12,7 @@ module.exports = function(req, res, next) {
     if (!error && response.statusCode == 200) {
       var result = JSON.parse(body)
       req.contentInfo = result
+      result.hotText = textForHotRate(result.ratio)
       result.ratio = parseInt(result.ratio * 100)
       result.card.view = result.card.view || 0
       result.distance = (result.distance / 1000).toFixed(1)
@@ -21,4 +22,13 @@ module.exports = function(req, res, next) {
       res.json({url: 'http://operation.renyan.cn/rest/share/position/' + cid, body: body})
     }
   })
+}
+
+function textForHotRate(rate) {
+  var hotTexts = ['冰冰冷', '一点也不热', '一般热', '炒鸡热', '热情的沙漠']
+  var index = parseInt(rate * hotTexts.length)
+  if (index >= hotTexts.length) {
+    index = hotTexts.length - 1;
+  }
+  return hotTexts[index]
 }
