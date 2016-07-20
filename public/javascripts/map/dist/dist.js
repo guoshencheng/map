5fc1b554c673b47dbc04e496a3ea11fadd028c00
guoshencheng <648772021@qq.com>
2016-07-20 17:45:46 +0800

    use mark to implements

--- public/javascripts/map/dist/dist.js
+++ public/javascripts/map/dist/dist.js
@@ -47,17 +47,14 @@
 	var TWEEN = __webpack_require__(1)
 	var p = window.point;
 	var map = new AMap.Map("map_container"); 
-	var animationPoint= {}
-	var list
-	var list2
-	var pointLayer
+	var list, list2, animation
 	var currentIndex = 0
 	clearMarks()
 	map.on('complete', function() {
 	  configureData(p)
 	  drawPoints()
 	  configurePoint()
-	  animationLine(list2[0], list2[1])
+	  animationLine()
 	})
 	map.setZoom(4)
 	
@@ -75,48 +72,17 @@
 	
 	function configurePoint() {
 	  var start = list2[0]
-	  pointLayer = new AMap.MassMarks([{lnglat: start.lnglat}], {
-	     url: '/images/point.png',
-	     anchor: new AMap.Pixel(20, 20),
-	     size: new AMap.Size(40, 40),
-	     opacity:1,
-	     cursor:'pointer',
-	     zIndex: 2 
-	  });
-	  pointLayer.setMap(map)
-	}
-	
-	var generateAnimationPoint = function() {
-	  if (p) {
-	    animationPoint = new AMap.Marker({
+	  animation = new AMap.Marker({
 	    map: map,
-			position: [p.lon, p.lat],
-	        icon: new AMap.Icon({            
-	            size: new AMap.Size(20, 20), 
-	            image: "/images/point.png",
-	            imageSize: new AMap.Size(20, 20),
-	            imageOffset: new AMap.Pixel(10, 10)
-	        })        
-	   });
-	  }
+	    position: start,
+	    icon: "/images/point.png",
+	    offset: new AMap.Pixel(-26, -13),
+	    autoRotation: true
+	  });
 	}
 	
-	var animationLine = function(p1, p2) {
-	  var tween = new TWEEN.Tween(p1.lnglat)
-	  var lnglat1 = new AMap.LngLat(p1.lnglat[0], p1.lnglat[1])
-	  var lnglat2 = new AMap.LngLat(p2.lnglat[0], p2.lnglat[1])
-	  var time = 1500 * (lnglat1.distance(lnglat2) / 1500000)
-	  tween.to(p2.lnglat, time)
-	  tween.onUpdate(function () {
-	    pointLayer.setData([{lnglat: this}])
-	  })
-	  tween.onComplete(function() {
-	    currentIndex ++
-	    if (currentIndex < list2.length - 1) {
-	      animationLine(list2[currentIndex], list2[currentIndex + 1])
-	    }
-	  })
-	  tween.start()
+	var animationLine = function() {
+	  animation.moveAlong(list2, 300000);
 	}
 	
 	function clearMarks() {
@@ -136,7 +102,7 @@
 	  getArray(p)
 	  function getArray(p) {
 	    list.push({lnglat: [p.lon, p.lat]})
-	    list2.push({lnglat: [p.lon, p.lat]})
+	    list2.push(new AMap.LngLat(p.lon, p.lat))
 	    if (p.nodes && p.nodes.length > 0) {
 	      p.nodes.forEach(function(node) {
 	        getArray(node)
