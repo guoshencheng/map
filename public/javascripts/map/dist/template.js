36c090a7044c8342f3fa54233ae23479e00400f4
guoshencheng <648772021@qq.com>
2016-07-20 15:13:19 +0800

    chang function to implements image aspect fill

--- public/javascripts/map/dist/template.js
+++ public/javascripts/map/dist/template.js
@@ -1,6 +1,5 @@
 var contentCard = document.getElementsByClassName('content_card')[0]
 if (contentCard) {
-var image = contentCard.getElementsByTagName('img')[0]
 var imageContainer = contentCard.getElementsByClassName('rect_image_container')[0]
 
 var templateCicle = document.getElementsByClassName('circle_template')
@@ -9,35 +8,6 @@ var templateTall = document.getElementsByClassName('tall_template')
 var templateFlat = document.getElementsByClassName('flat_template')
 var templateText = document.getElementsByClassName('text_template')
 var templatePicture = document.getElementsByClassName('picture_template')
-
-if (image.complete) {
-  adjustImageSize(image, 1)
-  centerImage(image, imageContainer)
-}
-
-image.onload = function() {
-  adjustImageSize(image, 1)
-  centerImage(image, imageContainer)
-}
-
-function centerImage(image, container) {
-  var left = (container.clientWidth - image.width) / 2
-  var top = (container.clientHeight - image.height) / 2
-  image.style.left = left + 'px'
-  image.style.top = top + 'px'
-  image.style.display = 'block'
-}
-  
-function adjustImageSize(image, tarScale) {
-  var scale = image.height / image.width
-  if (scale <= tarScale) {
-    image.height = imageContainer.clientHeight
-    image.width = image.height / scale
-  } else {
-    image.width = imageContainer.clientWidth
-    image.height = image.width * scale
-  }
-}
 
 if (templateCicle  && templateCicle.length > 0) {
   imageContainer.style.height = imageContainer.clientWidth + 'px'
@@ -60,8 +30,6 @@ if (templateText && templateText.length > 0) {
   imageContainer.style.height = 0 + 'px'
   var contentText = contentCard.getElementsByClassName('content_text')[0]
   contentText.style.marginTop = '0px'
-  image.style.display = 'none'
-  image.height = 0;
 }
 
 if (templatePicture && templatePicture.length > 0) {
