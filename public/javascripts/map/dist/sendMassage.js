5ee73a25dd48e7cb21c42cb550d9375e802c7332
guoshencheng <648772021@qq.com>
2016-07-22 17:50:56 +0800

    add ios function

--- public/javascripts/map/dist/sendMassage.js
+++ public/javascripts/map/dist/sendMassage.js
@@ -1,7 +1,10 @@
 function configureShare (shareConfig) {
   if (window.android && android.configureShare) {
     android.configureShare(JSON.stringify(shareConfig))
-  } 
+  }
+  if (window.webkit && webkit.messageHandlers && webkit.messageHandlers.message && webkit.messageHandlers.message.postMessage) {
+    window.webkit.messageHandlers.message.postMessage(JSON.stringify(shareConfig));
+  }
 }
 
 
