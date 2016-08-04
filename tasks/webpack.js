98e0cc7d5fd26011fffd0789fb6cc788811d4164
guoshencheng <648772021@qq.com>
2016-08-04 17:10:19 +0800

    add webpack-dev-set hot

--- tasks/webpack.js
+++ tasks/webpack.js
@@ -1,16 +1,19 @@
 
-var webpack = require('webpack-stream');
+var webpackGulp = require('webpack-stream');
+var webpack = require('webpack')
 var path = require('path')
 var webpackConfig = require('../webpack.config.js')
 var gutil = require("gulp-util");
 var WebpackDevServer = require("webpack-dev-server");
+
 var input = path.resolve(__dirname, '../public/javascripts/apps/**/src/index.js')
 var output = path.resolve(__dirname, '../public/javascripts/dist')
+var all = path.resolve(__dirname, '../public/javascripts/**/*.js')
 
 module.exports = function(gulp) {
   gulp.task('webpack', function() {
     return gulp.src(input)
-      .pipe(webpack(webpackConfig))
+      .pipe(webpackGulp(webpackConfig))
       .pipe(gulp.dest(output));
   });
 
@@ -19,12 +22,18 @@ module.exports = function(gulp) {
     myConfig.devtool = "eval"
     myConfig.debug = true
     new WebpackDevServer(webpack(myConfig), {
-      publicPath: "/" + myConfig.output.publicPath,
-      stats: { colors: true }
-    }).listen(8080, 'localhost', function(err) {
-      if (err) throw new gutil.PluginError("webpack-dev-server", err)
-      gutil.log("[webpack-dev-server]", "http://localhost:8080/webpack-dev-server/index.html")
+        publicPath: "/" + myConfig.output.publicPath,
+        stats: { colors: true}
+    }).listen(3000, "localhost", function(err) {
+        if(err) throw new gutil.PluginError("webpack-dev-server", err)
+        gutil.log("[webpack-dev-server]", "http://localhost:3000/webpack-dev-server/build/index.html")
     })
   })
+
+  gulp.task('watch', function() {
+    gulp.watch(all, ['webpack'])
+  })
+
+  gulp.task('webpack-server', ['webpack-dev-server', 'watch'])
 }
 
