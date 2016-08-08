522a8e04202d87a08b0b7bb2516c4b35a2efd10e
guoshencheng <648772021@qq.com>
2016-08-08 14:36:28 +0800

    add pencil and eraser and size icon

--- public/javascripts/apps/drawingboard/src/index.js
+++ public/javascripts/apps/drawingboard/src/index.js
@@ -149,6 +149,49 @@ var getContext = function() {
 }
 
 saveHistory()
-  
 
+var pencil = document.getElementById('pencil')
+var eraser = document.getElementById('eraser')
+var small = document.getElementById('small_size')
+var middle = document.getElementById('middle_size')
+var large = document.getElementById('large_size')
+var tool = [pencil, eraser]
+var size = [small, middle, large]
 
+var clicktool = function() {
+  var current = this
+  tool.forEach(function(child) {
+    if (child == current) {
+      child.style.backgroundColor = 'white'
+      child.style.color = 'black'
+    } else {
+      child.style.backgroundColor = 'black'
+      child.style.color = 'white'
+    }
+  })
+}
+
+var clickSize = function() {
+  var current = this
+  size.forEach(function(child) {
+    var icon = child.getElementsByClassName('size_icon')[0]
+    if (child == current) {
+      icon.style.backgroundColor = 'white'
+    } else {
+      icon.style.backgroundColor = 'black'
+    }
+  })
+}
+
+size.forEach(function(child) {
+  child.clickSize = clickSize
+  child.addEventListener('touchstart', clickSize)
+})
+
+tool.forEach(function(child) {
+  child.clicktool = clicktool
+  child.addEventListener('touchstart', clicktool)
+})
+
+pencil.clicktool()
+small.clickSize()
