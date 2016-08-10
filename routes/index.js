c300ee87f9532c90576d4a2847324777777f0ee3
guoshencheng <648772021@qq.com>
2016-08-10 15:43:17 +0800

    add app index page

--- routes/index.js
+++ routes/index.js
@@ -24,6 +24,10 @@ router.get('/drawingboard/index', function(req, res, next) {
   res.render('drawingboard/index', {title: '画猫'})
 })
 
+router.get('/drawingboard/index/app', function(req, res, next) {
+  res.render('drawingboard/index_app', {title: '猫粮计划'})
+})
+
 router.get('/drawingboard/work/:workId', drawingboard.fetchWork, function(req, res, next) {
   var work = req.renyan.work
   res.render('drawingboard/work', {title: 'xxxx的作品', work: work, me: false})
