db11f94dd2bc00178414d9c7986c48f161a054cc
guoshencheng <648772021@qq.com>
2016-08-12 16:19:02 +0800

    fix share url in test server

--- routes/maps.js
+++ routes/maps.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../config')
 var fetchContent = require('../middleware/fetchContent')
 var checkCidParam = require('../middleware/decodeCid')
 
@@ -11,13 +12,19 @@ router.get('/:id', checkCidParam, fetchContent, function(req, res, next) {
   renderMap(req, res, false)
 })
 
-var renderMap = function(req, res, share) {
-  var cid = req.cardId
+var renderMap = function(req, res, out) {
+  var cid = req.params.id
+  var share = {
+    text: contentInfo.profile.name + '分享了一条'+ contentInfo.hotText + '的人言，快来帮TA传播一下~',
+    image: contentInfo.card.pictureCut,
+    link: config.shareDomin + '/map/share/' + cid
+  }
   var contentInfo = req.contentInfo
   res.render('map', {
-    share: share,
+    in: out,
     contentInfo: contentInfo,
-    cid: req.params.id
+    cid: req.params.id,
+    share: share
   });
 }
 
