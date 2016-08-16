6ed4c935ce7112711522b442f3d9f774359c9d0e
guoshencheng <648772021@qq.com>
2016-08-16 11:35:52 +0800

    change function name

--- middleware/db/drawingboard/checkip.js
+++ middleware/db/drawingboard/checkip.js
@@ -3,8 +3,9 @@ var ipdb = require('../../../db/drawingboard').ips
 var checkip = function(req, res, next) {
   var ip = req.renyan.ip
   var workId = req.params.workId
-  ipdb.save(ip, workId, function(err, result, includes) {
+  ipdb.addLike(ip, workId, function(err, result, includes) {
     req.renyan.includes = includes
+    console.log(result)
     next()
   })
 }
