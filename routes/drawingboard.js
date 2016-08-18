1f45793935560c2f88713a70ece0fa8cb1883793
guoshencheng <648772021@qq.com>
2016-08-18 16:26:23 +0800

    add download

--- routes/drawingboard.js
+++ routes/drawingboard.js
@@ -4,25 +4,26 @@ var drawingboard = require('../middleware/drawingboard')
 var config = require('../config')
 var headers = require('../middleware/header')
 var checkip = require('../middleware/db/drawingboard/checkip')
-
+var defaultTitle = '十万喵星人拯救计划'
+var defaultText = '一起来参加人言十万喵星人拯救计划'
 router.post('/image', drawingboard.postImage)
 
 router.get('/index', function(req, res, next) {
   var share = {
-    text: '一起来参加人言十万喵星人拯救计划',
+    text: defaultText,
     image: 'http://7xpecj.com1.z0.glb.clouddn.com/2/renyan_logo.png',
     link: config.shareDomin + '/drawingboard/index/app'
   }
-  res.render('drawingboard/index', {title: '十万喵星人拯救计划', share: share, env: process.env.NODE_ENV})
+  res.render('drawingboard/index', {title: defaultTitle, share: share, env: process.env.NODE_ENV})
 })
 
 router.get('/index/app', function(req, res, next) {
   var share = {
-    text: '一起来参加人言十万喵星人拯救计划',
+    text: defaultText,
     image: 'http://7xpecj.com1.z0.glb.clouddn.com/2/renyan_logo.png',
     link: config.shareDomin + '/drawingboard/index/app'
   }
-  res.render('drawingboard/index_app', {title: '十万喵星人拯救计划', share: share, env: process.env.NODE_ENV})
+  res.render('drawingboard/index_app', {title: defaultTitle, share: share, env: process.env.NODE_ENV})
 })
 
 router.get('/like/:workId',headers.getRemoteAddress, checkip, drawingboard.likeWork)
@@ -34,7 +35,7 @@ router.get('/work/:workId', drawingboard.fetchWork, function(req, res, next) {
     image: work.image,
     link: config.shareDomin + '/drawingboard/work/' + req.params.workId
   }
-  res.render('drawingboard/work', {title: '十万喵星人拯救计划', work: work, me: false, share: share, env: process.env.NODE_ENV})
+  res.render('drawingboard/work', {title: defaultTitle, work: work, me: false, share: share, env: process.env.NODE_ENV})
 })
 
 router.get('/work/:workId/me', drawingboard.fetchWork, function(req, res, next) {
@@ -44,26 +45,26 @@ router.get('/work/:workId/me', drawingboard.fetchWork, function(req, res, next) 
     image: work.image,
     link: config.shareDomin + '/drawingboard/work/' + req.params.workId
   }
-  res.render('drawingboard/work', {title: '十万喵星人拯救计划', work: work, me: true, share: share, env: process.env.NODE_ENV})
+  res.render('drawingboard/work', {title: defaultTitle, work: work, me: true, share: share, env: process.env.NODE_ENV})
 })
 
 router.get('/works', drawingboard.fetchWorks, function(req, res, next) {
   var works = req.renyan.works
   var share = {
-    text: '一起来参加人言十万喵星人拯救计划',
+    text: defaultText,
     image: 'http://7xpecj.com1.z0.glb.clouddn.com/2/renyan_logo.png',
     link: config.shareDomin + '/drawingboard/works'
   }
-  res.render('drawingboard/works', {title: '十万喵星人拯救计划', works: works, share: share, env: process.env.NODE_ENV})
+  res.render('drawingboard/works', {title: defaultTitle, works: works, share: share, env: process.env.NODE_ENV})
 })
 
 router.get('/readme', function(req, res, next) {
    var share = {
-     text: '一起来参加人言十万喵星人拯救计划',
+     text: defaultText,
      image: 'http://7xpecj.com1.z0.glb.clouddn.com/2/renyan_logo.png',
      link: config.shareDomin + '/drawingboard/index/app'
    }
-   res.render('drawingboard/readme', {env: process.env.NODE_ENV, share: share, title:'十万喵星人拯救计划'})
+   res.render('drawingboard/readme', {env: process.env.NODE_ENV, share: share, title:defaultTitle})
 })
 
 module.exports = router
