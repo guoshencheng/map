64506f6439e0d373db707cac14894ad299344464
guoshencheng <648772021@qq.com>
2016-08-31 11:06:48 +0800

    add debug mode error

--- app.js
+++ app.js
@@ -40,26 +40,11 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    console.log(err.message)
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.render(app.get('env') === 'production' ?  'error' : 'debug/error', {
     message: err.message,
     error: err
   });
